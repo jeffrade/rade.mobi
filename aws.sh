7e92ff8ec759b9282cc7bd52486f04c289d1880e
Jeff Rade <jeffrade@gmail.com>
2017-12-09 19:34:06 -0600

    Adding devixgroup.com pages

--- aws.sh
+++ aws.sh
@@ -1,3 +1,5 @@
 aws s3 cp . s3://jeff.rade.me/ --recursive --exclude "aws.sh" --acl public-read --dryrun
 
 aws s3 cp . s3://rade.me/ --recursive --exclude "aws.sh" --exclude ".git/*" --exclude "README.md" --exclude "jeff/*" --exclude "lisa/*" --exclude "payton/*" --exclude "tyler/*" --acl public-read --dryrun
+
+aws s3 cp . s3://devixgroup.com/ --recursive --acl public-read --dryrun
