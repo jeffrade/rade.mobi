c987e2b427e86d6acf54f3fe68d575086c227823
Jeff Rade <jeffrade@gmail.com>
2020-12-30 15:31:18 -0600

    Update aws script to use 'aws s3 sync' and actually usable

--- aws.sh
+++ aws.sh
@@ -1,5 +1,36 @@
-aws s3 cp . s3://jeff.rade.me/ --recursive --exclude "aws.sh" --acl public-read --dryrun
+#!/bin/bash
+#
+# Deploys to existing S3 buckets using AWS CLI
 
-aws s3 cp . s3://rade.me/ --recursive --exclude "aws.sh" --exclude ".git/*" --exclude "README.md" --exclude "jeff/*" --exclude "lisa/*" --exclude "payton/*" --exclude "tyler/*" --acl public-read --dryrun
+set -e
 
-aws s3 cp . s3://devixgroup.com/ --recursive --acl public-read --dryrun
+if [ "$2" = '--dryrun' ]; then
+    DRYRUN="--dryrun"
+fi
+
+function s3_sync() {
+    SYNC_DIR=$1
+    SYNC_DOMAIN=$2
+    pushd "${SYNC_DIR}" > /dev/null
+    aws s3 sync . s3://"${SYNC_DOMAIN}" --exclude "private/*" --acl public-read ${DRYRUN}
+    popd > /dev/null
+}
+
+if [ "$1" = "jeff" ]; then
+    s3_sync "$1" "$1.rade.me"
+elif [ "$1" = "payton" ]; then
+    echo "Implement $1!"
+elif [ "$1" = "tyler" ]; then
+    echo "Implement $1!"
+elif [ "$1" = "lisa" ]; then
+    echo "Implement $1!"
+elif [ "$1" = "devixgroup" ]; then
+    s3_sync "$1.com" "$1.com"
+elif [ "$1" = "rade" ]; then
+    aws s3 sync . s3://"$1".me/ --exclude "*" --include "css/*" --include "js/*" --include "img/*" --include "index.html" --include "error.html" --acl public-read ${DRYRUN}
+else
+    echo "You did not provide a valid [sub]domain!"
+    exit 1
+fi
+
+echo "Completed!"
